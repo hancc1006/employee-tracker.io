76cbfa7643fc7f4e9c9586c6ea19e4aeb6b952ec
orphialtus <62411682+hancc1006@users.noreply.github.com>
2020-09-05 21:59:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Dragonleaf13!A",
+  password: "",
   database: "employee_db"
 });
 connection.connect(function (error) {
